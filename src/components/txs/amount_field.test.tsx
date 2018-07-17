de102793b86fdb83240f75432e950796aa7d3718
Alex Coventry <alx@mit.edu>
2018-07-17 14:40:49 -0400

    Change AmountField to take a callback function

--- src/components/txs/amount_field.test.tsx
+++ src/components/txs/amount_field.test.tsx
@@ -3,19 +3,21 @@ import * as ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import { AmountField } from './amount_field'
 
+const noOp = (x: number): void => { }
+
 describe('AmountField tests', () => {
     it('Accepts a sensible input, and renders', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<AmountField />, div);
+        ReactDOM.render(<AmountField callback={noOp} />, div);
     })
 
     it('Has an initial value of 0.', () => {
-        const field = shallow(<AmountField />)
+        const field = shallow(<AmountField callback={noOp} />)
         expect(field.find('input').props().value).toEqual('0')
     })
 
     it('Accepts a numeric input', () => {
-        const field = shallow(<AmountField />)
+        const field = shallow(<AmountField callback={noOp} />)
         field.find('input').simulate('change', { target: { value: '3.5' } })
         // Note, we need to grab the input field again, here, because the output
         // of `field.find('input')` is not changed by the `.simulate` call.
@@ -23,9 +25,16 @@ describe('AmountField tests', () => {
     })
 
     it('Rejects a non-numeric input', () => {
-        const field = shallow(<AmountField />)
+        const field = shallow(<AmountField callback={noOp} />)
         field.find('input').simulate('change', { target: { value: '3.5' } })
         field.find('input').simulate('change', { target: { value: 'hello' } })
         expect(field.find('input').props().value).toEqual('3.5')
+    })
+
+    it("Does what it's supposed to with its callback", () => {
+        var value = -1
+        const field = shallow(<AmountField callback={v => { value = v }} />)
+        field.find('input').simulate('change', { target: { value: '3.5' } })
+        expect(value).toEqual(3.5)
     })
 })
--- src/components/txs/amount_field.tsx
+++ src/components/txs/amount_field.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 
 var number_re = RegExp("^[0-9]+(\.[0-9]*)?$")  // Matches +ve decimals only.
 
+interface IAmountFieldProps { callback: (value: number) => void }
+
 /** Input field which only accepts non-negative numeric values
  *
  * Final value is in this.state.value, as a string.
  */
-export class AmountField extends React.Component {
+export class AmountField extends React.Component<IAmountFieldProps, {}> {
     state: { value: string };
-    constructor(props: any) {
+    propTypes: { callback: (value: number) => void }
+    constructor(props: IAmountFieldProps) {
         super(props);
         this.state = { value: '0' }
         this.handle_change = this.handle_change.bind(this)
@@ -21,6 +24,7 @@ export class AmountField extends React.Component {
         // Only accept input if it's a non-negative numeric value
         if (number_re.exec(target.value)) {
             this.setState({ value: target.value });
+            this.props.callback(parseFloat(target.value))
         }
     }
 
