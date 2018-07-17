5ae0062a7a004dad63b6f1747b6861b29ba2abc2
Alex Coventry <alx@mit.edu>
2018-07-17 12:27:58 -0400

    Remove misplaced transaction component

--- src/components/pay_dialog.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { Record } from 'immutable';
-import { AmountField } from './amount_field';
-import { SendButton } from './send_button';
-
-const payDialogRecord = ()
