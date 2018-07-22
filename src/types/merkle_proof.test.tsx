aa378ee4153f78a43dd5b755615a01a1189fee78
Alex Coventry <alx@mit.edu>
2018-07-21 20:49:18 -0400

    Export MerkleProof examples from types test

--- src/types/merkle_proof.test.tsx
+++ src/types/merkle_proof.test.tsx
@@ -4,22 +4,24 @@ import { HashValue } from './hash'
 import { MerkleProof } from './merkle_proof'
 import { MerkleProofLayer } from './merkle_proof_layer'
 
-const randomHash = () => new HashValue(randomHashString())
+export const randomHash = () => new HashValue(randomHashString())
 
-const firstLayer = new MerkleProofLayer({
+export const firstLayer = new MerkleProofLayer({
     child: false, left: randomHash(), right: randomHash()
 })
 
-const secondLayer = new MerkleProofLayer({
+export const secondLayer = new MerkleProofLayer({
     child: true, left: randomHash(), right: firstLayer.nextHash()
+})
+
+export const merkleProof = new MerkleProof({
+    proof: List([firstLayer, secondLayer]),
+    root: secondLayer.nextHash()
 })
 
 describe('Tests of Merkle proof types', () => {
     it('Accepts and stores a sensible Merkle proof', () => {
-        return new MerkleProof({
-            proof: List([firstLayer, secondLayer]),
-            root: secondLayer.nextHash()
-        }) && undefined
+        return merkleProof && undefined
     })
 
     it('Rejects bad Merkle proofs', () => {
