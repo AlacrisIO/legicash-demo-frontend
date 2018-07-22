f1e555f4eed973eb7757df381e2653d05332fd0d
Alex Coventry <alx@mit.edu>
2018-07-21 20:53:05 -0400

    Make it easier to mount components in the browser, for smoke testing
    Via `npm start`.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,23 @@
-/* import * as React from 'react';
+import * as React from 'react';
+/* import { render } from 'react-dom' */
+
+/*
  * import { ContractState } from './contract_state';
  * import { MainTxList } from './main_tx_list';
  * import { AddAccount } from './txs/add_account';
  * import { WalletList } from './wallet_list'; */
+
+import { Address } from '../types/address'
+import { PayDialog } from './txs/pay_dialog'
+
+/* tslint:disable:no-console */
+/* tslint:disable:no-var-keyword */
+export const App = () => {
+    const noOp = (a: number, t: Address) => {
+        console.log(JSON.stringify([a, t]))
+    }
+    return <PayDialog
+        from={new Address("0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")}
+        submitCallback={noOp} />
+}
+
