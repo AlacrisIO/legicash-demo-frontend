89256c04ffa78ffca7b829b4ab59995dd73701e2
Alex Coventry <alx@mit.edu>
2018-07-23 11:55:46 -0400

    Move MerkleProofDisplay code more in line with redux philosophy

--- src/components/merkle_proof.test.tsx
+++ src/components/merkle_proof.test.tsx
@@ -1,61 +1,35 @@
 import { mount } from 'enzyme'
 import * as React from 'react'
-import { MerkleProof } from '../types/merkle_proof';
 import { merkleProof } from '../types/merkle_proof.test'
-import { MerkleProofWait } from './merkle_proof'
+import { MerkleProofDisplay, MerkleProofWait } from './merkle_proof'
 
-it('Takes a MerkleProof promise, and waits for it', () => {
-    let completed = false
-    // Poll every 1 ms to check whether `completed` has been set.
-    const rProofPromise: () => Promise<MerkleProof> = () =>
-        new Promise(resolve => setTimeout(() =>
-            resolve((completed && merkleProof) || rProofPromise()), 1))
-    const proofPromise = rProofPromise()
-    const proofObject = <MerkleProofWait proofPromise={proofPromise} />
-    const proofDisplay = mount(proofObject)
-    expect(proofDisplay.find('.load-merkle').length).toEqual(1)
-    completed = true  // Simulate having loaded the proof
-    const wait = proofPromise.then(r => {
-        proofDisplay.update()  // Force a re-render
-        expect(proofDisplay.find('.load-merkle').length).toEqual(0)
-        expect(proofDisplay.find('table').length).toEqual(1)
+describe('MerkleProofDisplay tests', () => {
+    it('Renders a Merkle proof sanely', () => {
+        const m = mount(<MerkleProofDisplay proof={merkleProof} />)
+        const text = m.text()
+        expect(text).toContain('Root')
+        expect(text).toContain('Leaf')
     })
-    return Promise.all([wait])
 })
 
-it('Displays an error when the promise fails', () => {
-    // This is overall a fragile test, and probably implies that I need to
-    // redesign. Creates a proofPromise, which fails, and verifies that the
-    // failure message appears in the MerkleProofWait component afterwards.
-    //
-    // Jest likes to capture the unhandled error. Usually, this would be a good
-    // thing, but when you're explicitly testing failure it's a pain in the neck
-    const failureMessage = 'failure!'
-    let failed = false
-    const rProofPromise: any = () =>
-        (new Promise((resolve, reject) => setTimeout(() => {
-            // NB: If there's an exception raised on this line by jest, the
-            // failure is actually probably in checkReRender below.
-            if (failed) { reject(Error(failureMessage)) }
-            return resolve(rProofPromise())
-        }, 1)))
-    const proofPromise = rProofPromise()
-    expect(proofPromise).rejects.toEqual(Error(failureMessage))
-    const proofDisplay = mount(<MerkleProofWait proofPromise={proofPromise} />)
-    const awaitRender: () => Promise<boolean> = () =>
-        new Promise((resolve, reject) => {
-            const checkReRender = () => {
-                if (proofDisplay.render().toString().includes(failureMessage)) {
-                    resolve(true)  // Update has occurred as expected
-                } else { resolve(false) }
-            }
-            // This is fragile... Too long, and jest raises proofPromise's 
-            // unhandled rejection as an exception. Too short, and the
-            // component doesn't have time to re-render
-            setTimeout(checkReRender, 0.01)
-        })
-    const caughtPromise = proofPromise.catch((e: Error) => awaitRender())
-    failed = true  // Simulate failure
-    expect(caughtPromise).resolves.toEqual(true)  // See checkReRender
-    return caughtPromise
+/* XXX: These are just boilerplate. Really needs some integration tests with the
+ * promise machinery. That will have to wait until the overall architecture is
+ * clearer.
+ */
+
+describe('MerkleProofWait tests', () => {
+    it('Renders a MerkleProof, when provided', () => {
+        const proofObject =
+            <MerkleProofWait result={merkleProof} eventInfo="foo" />
+        expect(mount(proofObject).find('table.merkleTable').length).toEqual(1)
+    })
+    it('Renders an error, when provided', () => {
+        const proofObject =
+            <MerkleProofWait result={Error("fail!")} eventInfo="f" />
+        expect(mount(proofObject).find('.load-merkle-error').length).toEqual(1)
+    })
+    it('Renders a spinner, when neither is available', () => {
+        const proofObject = <MerkleProofWait result={null} eventInfo="f" />
+        expect(mount(proofObject).find('.load-merkle').length).toEqual(1)
+    })
 })
--- src/components/merkle_proof.tsx
+++ src/components/merkle_proof.tsx
@@ -13,23 +13,28 @@ const alignCSS: React.CSSProperties = { 'textAlign': 'center' }
 /* Red styling if true, none otherwise */
 const hashColor = (c: boolean) => (c ? targetCSS : undefined)
 
+const truncateHash = (h: HashValue) => h.toRawString().substring(0, 10)
+
 /* Start and end rows of table, like | Root |  <root hash> | */
 const boundaryLevel = (name: string, hash: HashValue) =>
     <tr>
-        <td>{name}</td><td colSpan={2} style={targetCSS}>{hash.toString()}</td>
+        <td>{name}</td>
+        <td colSpan={2} style={targetCSS}>{truncateHash(hash)}</td>
     </tr>
 
-/* Internal level of Merkle tree. Red for target node, plain for sibling */
+/* Internal level of Merkle tree. Target nodes with hashColor style, sibling
+ * nodes plain
+ */
 const renderLayer = (l: MerkleProofLayer, idx: number) =>
     <tr key={idx}>
         <td />
-        <td style={hashColor(!l.child)}> {l.left.toString()}</td>
-        <td style={hashColor(l.child)}> {l.right.toString()}</td>
+        <td style={hashColor(!l.child)}> {truncateHash(l.left)}</td>
+        <td style={hashColor(l.child)}> {truncateHash(l.right)}</td>
     </tr>
 
 export interface IMerkleProofDisplay { proof: MerkleProof }
 
-/** Renders a Merkle proof */
+/** Renders a MerkleProof for display to the user */
 export const MerkleProofDisplay = ({ proof }: IMerkleProofDisplay) =>
     <table className="merkleTable" style={alignCSS} >
         <tbody>
@@ -40,28 +45,23 @@ export const MerkleProofDisplay = ({ proof }: IMerkleProofDisplay) =>
     </table>
 
 interface IMerkleProofWait {
-    proofPromise: Promise<MerkleProof>
+    /** Description of the data for which a proof is being sought */
+    eventInfo: React.ReactElement<any> | string;
+    /** Holds the proof result, or an error, if available. */
+    result: MerkleProof | Error | null | undefined | false
 }
 
-/** Renders a Merkle proof, once it's available from a promise */
-export class MerkleProofWait extends React.Component<IMerkleProofWait, {}> {
-    public state: { proof?: MerkleProof; error?: Error }
-    constructor(props: IMerkleProofWait) {
-        super(props)
-        props.proofPromise
-            .then(r => this.setState({ proof: r }))
-            .catch(e => {
-                this.setState({ error: e })
-            })
-    }
-    public render() {
-        if (this.state && this.state.proof) {
-            return <MerkleProofDisplay proof={this.state.proof} />
-        } else if (this.state && this.state.error) {
-            return <p>{this.state.error.message}</p>
-        } else {
-            return <Spinner name="circle"
-                overrideSpinnerClassName="load-merkle" />
-        }
-    }
-}
+/**
+ * Renders a Merkle proof, once it's available via relevant redux state
+ *
+ * Or reports an error, if that happens, and spins until one or the other.
+ */
+export const MerkleProofWait = ({ eventInfo, result }: IMerkleProofWait) =>
+    <div className="merkleProofWait">
+        <p>"Proof for " {eventInfo}</p>
+        {(result instanceof MerkleProof) && <MerkleProofDisplay proof={result} />}
+        {(result instanceof Error) &&
+            <p className="load-merkle-error">"Error: " {result.message}</p>}
+        {(!result) &&
+            <Spinner name="circle" overrideSpinnerClassName="load-merkle" />}
+    </div>
