8631f3f7c01785266adfdb84c12a3b9312024fd1
Alex Coventry <alx@mit.edu>
2018-07-25 14:34:09 -0400

    Move some example constructions to a common file for reuse

--- src/types/merkle_proof.test.tsx
+++ src/types/merkle_proof.test.tsx
@@ -1,23 +1,7 @@
 import { List } from 'immutable'
-import { randomHashString } from './common'
-import { HashValue } from './hash'
+/* tslint:disable:ordered-imports */
+import { randomHash, firstLayer, secondLayer, merkleProof } from './common'
 import { MerkleProof } from './merkle_proof'
-import { MerkleProofLayer } from './merkle_proof_layer'
-
-export const randomHash = () => new HashValue(randomHashString())
-
-export const firstLayer = new MerkleProofLayer({
-    child: false, left: randomHash(), right: randomHash()
-})
-
-export const secondLayer = new MerkleProofLayer({
-    child: true, left: randomHash(), right: firstLayer.nextHash()
-})
-
-export const merkleProof = new MerkleProof({
-    proof: List([firstLayer, secondLayer]),
-    root: secondLayer.nextHash()
-})
 
 describe('Tests of Merkle proof types', () => {
     it('Accepts and stores a sensible Merkle proof', () => {
