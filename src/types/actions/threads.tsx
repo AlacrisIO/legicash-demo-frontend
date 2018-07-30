28d1d1d595acaf9b5c6fe9684e69139a18702094
Alex Coventry <alx@mit.edu>
2018-07-30 17:21:29 -0400

    Comments for readability

--- src/types/actions/threads.tsx
+++ src/types/actions/threads.tsx
@@ -1,5 +1,6 @@
 import { Action, IActionType, IServerResponse } from './base_actions'
 
+/** Base type for thread actions */
 export interface IThreadType extends IActionType {
     /** ID number for the server thread, so we can check back for completion */
     readonly threadNumber: number;
@@ -16,7 +17,7 @@ export const threadResponse =
     (threadNumber: number, nextAction: IServerResponse): IThreadResponse => (
         { type: Action.THREAD_RESPONSE, threadNumber, nextAction })
 
-
+/** Sent when it's time to check a thread again. */
 export interface ICheckThread extends IThreadType {
     readonly type: Action.CHECK_THREAD
 }
