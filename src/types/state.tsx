72f3aee125362b5e9a314b18f1222c773800c3ca
Alex Coventry <alx@mit.edu>
2018-07-30 14:09:56 -0400

    Update state on server validation of deposit transaction

--- src/types/state.tsx
+++ src/types/state.tsx
@@ -4,7 +4,7 @@ import { Guid } from './guid'
 import { List, Map, Record } from './immutable'
 import { Transaction } from './tx'
 import { Wallet } from './wallet'
-
+/* tslint:disable:object-literal-sort-keys */
 const defaultValues = {
     /** Wallets known to the front end */
     accounts: Map<Address, Wallet>(),
@@ -14,8 +14,9 @@ const defaultValues = {
     displayedAccounts: List<Address>(),
     /** Transactions, indexed by various characteristics */
     txByFromAddress: Map<Address, List<Guid>>(),
-    txByGUID: Map<Guid, Transaction>(),
     txByToAddress: Map<Address, List<Guid>>(),
+    /** Actual store for transactions */
+    txByGUID: Map<Guid, Transaction>(),
 }
 
 export class UIState extends Record(defaultValues) {
@@ -23,23 +24,22 @@ export class UIState extends Record(defaultValues) {
         // XXX: Check consistency between tx* members, addresses, etc.
         super(props)
     }
-    /** State with wallet added */
-    public addWallet(username: string, address: Address, onchainBalance?: 0,
-        offchainBalance?: 0): this {
+    /** State with wallet added, if necessary. */
+    public addWallet(username: string, address: Address): this {
         return this.updateIn(
             ['accounts', address],
             (w: Wallet) => {
                 const txs = List<Guid>(this.txByFromAddress.get(address).concat(
                     this.txByToAddress.get(address)))
-                return (w || new Wallet({
-                    address, offchainBalance, onchainBalance, txs, username
-                }))
+                return (w || new Wallet({ address, txs }))
+                    .set('username', username)  // Allow update of username
             })
     }
     /** State with tx added */
     public addTx(tx: Transaction): this {
         // Add tx to map, ensuring compatibility with any extant tx
         const updateTx = (otx: Transaction | undefined) =>
+            // XXX: Fail more gracefully on contradiction? Some kind of warning?
             otx && (tx.assertSameTransaction(otx) || tx)
         const updateGUID = (guid: Guid | undefined) => tx.localGUID
         const updateWallet = (a: Address) => (w: Wallet | undefined) =>
@@ -54,6 +54,13 @@ export class UIState extends Record(defaultValues) {
                     [[tx.to], updateWallet(tx.to)],
                     [[tx.from], updateWallet(tx.from)]
                 ])
-            ]])
+            ]
+        ])
     }
+    /** State with tx marked as rejected */
+    public rejectTx(tx: Transaction): this {
+
+        return this
+    }
+
 }
--- src/types/wallet.tsx
+++ src/types/wallet.tsx
@@ -1,17 +1,21 @@
 import { emptyAddress } from './address'
+import { balanceUpdate, Chain } from './chain'
 import { Guid } from './guid'
-import { List, Map, Record } from './immutable'
+import { List, Map, Record, Set } from './immutable'
 import { Transaction } from './tx'
 
+/* tslint:disable:object-literal-sort-keys */
 const defaultValues = {
     /** Cryptographic address for this account */
     address: emptyAddress,
     /** Balance in this address in the side chain. Cannot be negative. */
     offchainBalance: 0,
     /** Balance in this address on-chain. Cannot be negative. */
-    onchainBalance: 0,
+    // XXX: We are effectively agnostic about onchainBalance, at this point.
+    onchainBalance: Infinity,
     /** Known transactions for this account. Append only! */
     txs: List<Guid>(),
+    txSet: Set<Guid>(),
     /** Human-readable username for this account */
     username: ''
 }
@@ -23,16 +27,43 @@ export class Wallet extends Record(defaultValues) {
         if (this.offchainBalance < 0) { throw Error("Offchain balance negative!") }
         // XXX: Check that all transactions belong to `address`?
     }
-    public addTx(txID: Guid, txs: Map<Guid, Transaction>): Wallet {
+    /** Wallet with this tx added */
+    public addTx(txID: Guid, txs: Map<Guid, Transaction>): this {
+        const tx = txs.get(txID)
+        const updates: Array<[string[], (a: any) => any]> = [
+            // To be applied in a big batch at the end
+            [['txs'], this.updateTxs(txID, tx, txs)],
+            [['txSet'], (s: Set<Guid>) => s.add(txID)],
+        ]
+        if (!this.txSet.has(txID)) {
+            // First time we're seeing this tx; optimistically update balances.
+            updates.push(
+                [['offchainBalance'], this.updateBalance(tx, Chain.Side)],
+                [['onchainBalance'], this.updateBalance(tx, Chain.Main)])
+        }
+        return this.multiUpdateIn(updates)
+    }
+    /** Function for updating the tx list with the given txID. */
+    private updateTxs(txID: Guid, tx: Transaction, txs: Map<Guid, Transaction>
+    ): (l: List<Guid>) => List<Guid> {
+        /* Logic for ensuring txs are sorted... */
+        // XXX: Add more sorting options, not just reverse chronological
         const txDate = (t: Guid) => {
-            const tx = txs.get(t)
             if (tx) { return tx.creationDate }
             throw Error(`Unrecorded transaction: ${t.guid}`)
         }
         const cmp = (t1: Guid, t2: Guid) => {
-            const [d1, d2] = [txDate(t1), txDate(t2)]
+            const [d1, d2] = [txDate(t1), txDate(t2)] as [Date, Date]
+            if ((d1 !== undefined) || (d2 !== undefined)) {
+                throw Error(`Date undefined in ${t1} or ${t2}!`)
+            }
             return ((d1 > d2) && 1) || ((d1 < d2) && -1) || 0
         }
-        return this.update('txs', (l: List<Guid>) => l.push(txID).sort(cmp))
+        // XXX: This is n*log(n), needs to be log(n) (binary search.)
+        return l => List(l.push(txID).sort(cmp))
+    }
+    /** Function for updating balance, given tx direction. */
+    private updateBalance(tx: Transaction, c: Chain): (balance: number) => number {
+        return balance => balance + balanceUpdate(tx, this.address, c)
     }
 }
