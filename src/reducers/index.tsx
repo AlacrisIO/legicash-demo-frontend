17c0d139a1df459a5df64ec99eb8111c3de309f0
Alex Coventry <alx@mit.edu>
2018-07-30 14:10:29 -0400

    Reducers for deposit workflow

---
+++ src/reducers/index.tsx
@@ -0,0 +1,22 @@
+/** Redux reducer logic */
+
+import * as Actions from '../types/actions'
+import { UIState } from '../types/state'
+
+export const deposit = (state: UIState, action: Actions.IActionType)
+    : UIState => {
+    switch (action.type) {
+        // Deposit-related actions; see ../types/actions/deposit.tsx
+        case Actions.Action.ADD_ADDRESS:
+            const addAction = (action as Actions.IAddAddress)
+            return state.addWallet(addAction.username, addAction.address)
+        case Actions.Action.MAKE_DEPOSIT:
+            return state.addTx((action as Actions.IMakeDeposit).tx)
+        case Actions.Action.DEPOSIT_VALIDATED:
+            return state.addTx((action as Actions.IDepositValidated).tx)
+        case Actions.Action.DEPOSIT_FAILED:
+            return state.rejectTx((action as Actions.IDepositFailed).tx)
+    }
+    return state
+}
+
