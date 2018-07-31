10f4eaa4baa63638e908285feb0b3d6386ddc85b
Alex Coventry <alx@mit.edu>
2018-07-31 18:48:03 -0400

    Wire up AddAccount and WalletList into root App.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,20 @@
-import { Set } from 'immutable'
 import * as React from 'react';
 
 /*
  * import { ContractState } from './contract_state';
  * import { MainTxList } from './main_tx_list';
- * import { AddAccount } from './txs/add_account';
  * import { WalletList } from './wallet_list'; */
 // import { merkleProof } from '../types/common'
 // import { MerkleProofDisplay } from './merkle_proof'
 
 import { AddAccount } from './add_account'
+import { WalletList } from './wallet_list'
 
 /* tslint:disable:no-console */
 /* tslint:disable:jsx-no-lambda */
 /* tslint:disable:no-var-keyword */
 export const App = () =>
-    <AddAccount presentAccounts={Set()}
-        add={e => { console.log('result', JSON.stringify(e)) }} />
+    <div>
+        <AddAccount />
+        <WalletList />
+    </div>
