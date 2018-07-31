d83156fd448dcc903d694321d876b771329bb2c2
Alex Coventry <alx@mit.edu>
2018-07-31 18:46:09 -0400

    Simple Wallet component

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -1,3 +1,29 @@
-import * as React from 'react';
-import { TxList } from './tx_list';
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { Address } from '../types/address'
+import { UIState } from '../types/state'
+// import { TxList } from './tx_list'  // Will need this later...
+import { Wallet as WalletType } from '../types/wallet'
 
+export const DumbWallet = ({ wallet }: { wallet: WalletType }) =>
+    <div>
+        <h2>Username: {wallet.username}</h2>
+        <p>Address: {wallet.address.toString()}</p>
+        <p>Offchain balance: {wallet.offchainBalance}</p>
+        <p>Onchain balance: {wallet.onchainBalance}</p>
+        <p>Transaction list: To be completed</p>
+    </div>
+
+export const Wallet =
+    ({ address }: { address: Address }) =>
+        connect(
+            (state: UIState) => {
+                const wallet = state.accounts.get(address)
+                if (wallet === undefined) {
+                    throw Error(`Wallet for unknown address! \
+${address}`)
+                }
+                return { wallet }
+            },
+            (dispatch: (a: any) => any) => ({ /* empty */ })
+        )(DumbWallet) 
