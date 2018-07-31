e36082425d35cc26d735a992b852064442633fb2
Alex Coventry <alx@mit.edu>
2018-07-31 18:46:17 -0400

    Simple wallet list component

--- src/components/wallet_list.tsx
+++ src/components/wallet_list.tsx
@@ -1,2 +1,17 @@
-import * as React from 'react';
-import { Wallet } from './wallet';
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { Address } from '../types/address'
+import { List } from '../types/immutable'
+import { UIState } from '../types/state'
+import { Wallet } from './wallet'
+
+export const DumbWalletList = ({ wl }: { wl: List<Address> }) =>
+    <div>{...wl.map((address: Address, i: number) => {
+        const WalletComponent = Wallet({ address })
+        return <WalletComponent key={i} />
+    }).toArray()}</div>
+
+export const WalletList = connect(
+    (state: UIState) => ({ wl: state.displayedAccounts }),
+    (dispatch: (a: any) => any) => ({ /* empty */ })
+)(DumbWalletList)
