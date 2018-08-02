9e6c3e1d5ef08e124eb607d0e5dbc6727ac17cb7
Alex Coventry <alx@mit.edu>
2018-08-02 16:52:12 -0400

    Deposit saga *almost works*
    At least, all the saga machinery is working.

---
+++ src/sagas/index.test.tsx
@@ -0,0 +1,60 @@
+import { fromJS, is } from 'immutable'
+import { expectSaga } from 'redux-saga-test-plan'
+
+import { makeDeposit } from '../sagas'
+import { get, post } from '../server/common'
+
+import * as Actions from '../types/actions'
+// import { depositFailed } from '../types/actions';
+import { Address } from '../types/address'
+import { Chain } from '../types/chain'
+import { HashValue } from '../types/hash'
+import { Transaction } from '../types/tx'
+
+const address = new Address('0x5050505050505050505050505050505050505050')
+const tx = new Transaction({
+    amount: 5, dstChain: Chain.Side, from: address,
+    srcChain: Chain.Main, to: address
+})
+const hash = new HashValue(
+    '0x0000000000000000000000000000000000000000000000000000000000000000')
+
+const depositMocks = {
+    call(effect: any, next: any): any {
+        switch (effect.fn) {
+            case post:
+                if (!is(fromJS(effect.args), fromJS(
+                    ["deposit", { address, "amount": tx.amount }]))) {
+                    // Throwing has no effect, here!
+                    /* tslint:disable:no-console */
+                    console.log(`Bad post call! ${JSON.stringify(effect)}`)
+                }
+                return { result: "api/thread?id=5" }
+            case get:
+                return {
+                    /* tslint:disable:object-literal-sort-keys */
+                    user_account_state: {
+                        address, user_name: "Alice", balance: 50
+                    },
+                    side_chain_tx_revision: 256,
+                    main_chain_confirmation: {
+                        transaction_hash: hash.toString(),
+                        transaction_index: 651,
+                        block_number: 50,
+                        block_hash: hash.toString()
+                    }
+
+                }
+            default:
+                return next()
+        }
+    }
+}
+
+describe('Deposit saga tests', () => {
+    it('Hits the deposit endpoint, then the thread endpoint and returns the \
+result', () => {
+            return expectSaga(makeDeposit, Actions.makeDeposit(address, tx))
+                .provide(depositMocks).run()
+        })
+})
--- src/sagas/index.tsx
+++ src/sagas/index.tsx
@@ -1,38 +1,56 @@
-// import { delay } from 'redux-saga'
-// import { call, put } from 'redux-saga/effects'
-// import * as Actions from '../types/actions'
-// import { Address } from '../types/address'
-// import { Transaction } from '../types/tx'
-// // import { Wallet } from '../types/wallet'
-// import { get, post, readThread, IThreadResponse } from '../server/common'
-// 
-// type depositResponse = Actions.IDepositValidated | Actions.IDepositFailed
-// 
-// function* depositFailed(address: Address, tx: Transaction, e: Error) {
-//     yield put(Actions.depositFailed(address, tx, e))
-// }
-// 
-// function* hitServer(args: any[], errAction: (e: Error) => Actions.Action) {
-//     try { return yield* call(...(args as any)) } catch (e) {
-//     }
-// }
-// 
-// 
-// function* make_deposit(action: Actions.IMakeDeposit): depositResponse {
-//     const address = action.address
-//     const amount = action.tx.amount
-//     let threadResponse: null | IThreadResponse = null
-//     try {
-//         threadResponse = yield call(post, 'deposit', { address, amount })
-//         // XXX: Fail more gracefully: exponential backoff for a while?
-//     } catch (e) { yield* depositFailed(address, action.tx, e) }
-//     const threadNumber = readThread(threadResponse as IThreadResponse)
-//     let delayTimeMS = 1
-//     for (let checkIdx: number = 0; checkIdx < 12; checkIdx++) {
-//         yield delay(delayTimeMS)
-//         try {
-//             // response = yield call(get, 'thread', { id: threadNumber })
-//         } catch (e) { /* yield* depositFailed(add */ }
-//     }
-// 
-// 
+import { delay } from 'redux-saga'
+import { call, put } from 'redux-saga/effects'
+
+import {
+    get, IThreadResponse, post, readThread, resultPending
+} from '../server/common'
+import * as Actions from '../types/actions'
+import { depositFailed } from '../types/actions';
+import { Address } from '../types/address'
+import { HashValue, emptyHash } from '../types/hash'
+import { Transaction } from '../types/tx'
+
+/** Hit server with given args, and report deposit failure on exception */
+const serverWithErrorHandling = (address: Address, tx: Transaction) =>
+    function* (...args: any[]) {
+        try { return yield (call as any)(...args) } catch (e) {
+            yield put(Actions.depositFailed(address, tx, e))
+        }
+    }
+
+/* See https://gitlab.com/legicash/legicash-facts/blob/endpoints-demo/src/endpoints/endpoints.md#depositwithdrawal-threads */
+function* awaitThread(server: any, threadResponse: IThreadResponse) {
+    const threadNumber = readThread(threadResponse as { result: string })
+    let delayTimeMS = 1  // Initial delay time before checking
+    let threadCheck
+    for (let checkIdx: number = 0; checkIdx < 12; checkIdx++) {  // Wait ~8s
+        yield call(delay, delayTimeMS)
+        delayTimeMS *= 2  // Exponential backoff after each failed try
+        threadCheck = yield* server(get, 'thread', { id: threadNumber })
+        if (!resultPending(threadCheck)) { break }
+    }
+    return threadCheck
+}
+
+const updateHash = (rawHash: string) => (h: HashValue) => {
+    const newHash = new HashValue(rawHash)
+    return (h: HashValue): HashValue => {
+        if ((!h.equals(emptyHash)) && (!h.equals(newHash))) {
+            throw Error(`Conflicting hashes! ${h} vs ${newHash}`)
+        }
+        return newHash
+    }
+}
+
+export function* makeDeposit(action: Actions.IMakeDeposit) {
+    const address = action.address
+    const amount = action.tx.amount
+    const server = serverWithErrorHandling(address, action.tx)
+    const threadResponse = yield* server(post, 'deposit', { address, amount })
+    const result: any = yield awaitThread(server, threadResponse)
+    if (resultPending(result)) {
+        return depositFailed(address, action.tx, Error("Timed out!"))
+    }
+    // Update the transaction with the new information
+    return Actions.depositValidated(address, action.tx, result)
+}
