608d9c961a02312e6aaa78c163027aca11974dbe
Alex Coventry <alx@mit.edu>
2018-08-01 23:48:45 -0400

    Address can initialize from another Address

--- src/types/address.tsx
+++ src/types/address.tsx
@@ -6,14 +6,18 @@ export const addressRegexp = RegExp("^0x([0-9A-Fa-f]{2}){20}$|^$");
  */
 export class Address {
     protected readonly address: string
-    constructor(address: string) {
+    constructor(address: string | Address) {
+        address = (address as Address).address || (address as string)
         if (!addressRegexp.exec(address)) {
             throw Error("Bad Ethereum address: " + address);
         }
         this.address = address;
     }
     public toString(): string { return this.address }
-    public equals(o: Address): boolean { return this.address === o.address }
+    public equals(o: Address): boolean {
+        return this.address === o.address
+
+    }
 }
 
 export const emptyAddress = new Address('')
