3cdfb57ba65cdd77934b991c58a5768e3c0cb7ec
Alex Coventry <alx@mit.edu>
2018-08-01 23:49:20 -0400

    Debug AddAccount

--- src/components/add_account.test.tsx
+++ src/components/add_account.test.tsx
@@ -1,14 +1,16 @@
 import { mount } from 'enzyme'
-import { Set } from 'immutable'
+import { List } from 'immutable'
 import * as React from 'react';
 import { addresses } from '../server/ethereum_addresses'
 import { Address } from '../types/address'
-import { DumbAddAccount, knownAddresses } from './add_account'
+import { DumbAddAccount, knownAddresses, name } from './add_account'
 
 describe('Tests for add account dialog', () => {
     let v: Address | undefined
     const add = (e: Address) => { v = e }
-    const m = mount(<DumbAddAccount presentAccounts={Set()} add={add} />)
+    const sortedAddresses = List(knownAddresses.sortBy(name))
+    const m = mount(
+        <DumbAddAccount displayedAddresses={sortedAddresses} add={add} />)
     it('Renders without crashing', () => {
         const defaultOption = m.find('.accountDefaultOption')
         expect(defaultOption.length).toBe(1)
@@ -19,10 +21,18 @@ describe('Tests for add account dialog', () => {
         form.simulate('submit')
         expect(v).toBe(undefined)
     })
+    const initialIndex = sortedAddresses.size - 1  // Choose bottom entry
+    const initialAddress = new Address(sortedAddresses.get(initialIndex))
     it('Submits when something is chosen', () => {
-        const change = { target: { value: knownAddresses.first().toString() } }
-        m.find('select').simulate('change', change)
+        // Following https://github.com/airbnb/enzyme/issues/389#issuecomment-401582776
+        const option = m.find(`option [value="${initialAddress}"]`)
+        const optionNode = option.getDOMNode() as any
+        optionNode.selectedIndex = initialIndex + 1
+        option.simulate('change', { target: optionNode })
         form.simulate('submit')
-        expect((v as Address).equals(knownAddresses.first())).toBe(true)
+        expect((v as Address).equals(initialAddress)).toBe(true)
+    })
+    it('Moves to the next entry up, when there is one, if at bottom', () => {
+        expect((m.state() as any).optIdx).toBe(sortedAddresses.size - 1)
     })
 })
--- src/components/add_account.tsx
+++ src/components/add_account.tsx
@@ -1,82 +1,104 @@
 import { List, Set } from 'immutable'
 import * as React from 'react'
 import { connect } from 'react-redux'
-import { addresses } from '../server/ethereum_addresses'
+import { addresses as allAddresses } from '../server/ethereum_addresses'
 import { addAddress } from '../types/actions'
 import { Address, emptyAddress } from '../types/address'
 import { UIState } from '../types/state'
 
-export const knownAddresses = Set(Object.keys(addresses).map(
-    n => addresses[n]))
+export const knownAddresses = Set(Object.keys(allAddresses).map(
+    n => allAddresses[n]))
 export const addressNames: { Address: string } = {} as any
-Object.keys(addresses).forEach(n => addressNames[addresses[n]] = n)
+Object.keys(allAddresses).forEach(n => addressNames[allAddresses[n]] = n)
 
-const defaultOption = (
-    <option key={-1} className="accountDefaultOption addOpt">
-        Please pick an account to create
+const defaultOption: JSX.Element = (
+    <option key={-1} className="accountDefaultOption addOpt"
+        value={emptyAddress.toString()}>
+        Please pick a wallet to display
     </option>
 )
 
-const name = (a: Address) => addressNames[a.toString()]
+export const name = (a: Address) => addressNames[a.toString()]
 
 /* Render available addresses as options for a dropdown menu */
-const addressOptions = (presentAccounts: Set<Address>) =>
-    List([defaultOption]).concat(
-        List(knownAddresses.subtract(presentAccounts))
-            .sortBy(name) // Sort names
-            .map((address, i: number) =>
-                <option key={i} value={address.toString()} className="addOpt">
-                    {name(address)}
-                </option>))
+const addressOptions = (addresses: List<Address>) =>
+    List<JSX.Element>([defaultOption]).concat(
+        addresses.map((address: Address, i: number): JSX.Element =>
+            <option key={i} value={address.toString()} className="addOpt">
+                {name(address)}
+            </option>))
 
 export interface IAddAccount {
     /** Callback when account-add is requested */
     add: ((a: Address) => void);
     /** Accounts which are already listed in the UI */
-    presentAccounts: Set<Address>
+    displayedAddresses: List<Address>;
 }
 
+export interface IAddAccountState { optIdx: number }
+/* tslint:disable:max-classes-per-file */
+class BaseComponent extends React.Component<IAddAccount, IAddAccountState>{ }
+
 /** Form for adding an account to the side chain */
-export class DumbAddAccount extends React.Component<IAddAccount, {}> {
-
-    public state: { address: Address } = { address: emptyAddress }
-
+export class DumbAddAccount extends BaseComponent {
+    public state: IAddAccountState = { optIdx: 0 }
     public constructor(props: IAddAccount) {
         super(props)
         this.onChange = this.onChange.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
     }
-
     public onChange(e: React.ChangeEvent<HTMLSelectElement>) {
-        const address = new Address((e.target as HTMLSelectElement).value)
-        this.setState({ address })
+        const optIdx = (e.target as HTMLSelectElement).selectedIndex
+        if (optIdx === undefined) {
+            throw Error("No index into <select> options!")
+        }
+        this.setState({ optIdx })
     }
-
     public onSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        if (this.state.address !== emptyAddress) {
-            this.props.add(this.state.address)
+        if (this.state.optIdx !== 0) {
+            const address = this.currentDisplayedAddress()
+            if (address.equals(emptyAddress)) {
+                /* tslint:disable:no-console */
+                // XXX: This happened once, but I haven't been able to reproduce
+                throw Error(`OptIdx is ${this.state.optIdx}, which is outside \
+the range for the current options list, \
+${this.props.displayedAddresses.toArray()} of length \
+${this.props.displayedAddresses.size}.`)
+            } else {
+                this.props.add(address)
+            }
+        }
+        if (this.state.optIdx === this.props.displayedAddresses.size) {
+            // Move to the next-highest entry, if at the bottom, else next-lowest
+            this.setState({ optIdx: Math.max(0, this.state.optIdx - 1) })
         }
     }
-
+    public currentDisplayedAddress(): Address {
+        return ((this.state.optIdx > 0)
+            && this.props.displayedAddresses.get(this.state.optIdx - 1))
+            || emptyAddress
+    }
     public render() {
         return (
             <div className="addAccount">
                 <form className="addAccountMenue" onSubmit={this.onSubmit}>
                     Add account:
-                <select onChange={this.onChange}>
-                        {addressOptions(this.props.presentAccounts)}
+                    <select onChange={this.onChange}
+                        value={this.currentDisplayedAddress().toString()} >
+                        {addressOptions(this.props.displayedAddresses)}
                     </select>
                     <input type="submit" value="Submit" />
                 </form>
-            </div>
+            </div >
         )
     }
 }
 
 export const AddAccount = connect(
     (state: UIState) => ({
-        presentAccounts: Set(state.displayedAccounts)
+        displayedAddresses: List(
+            knownAddresses.subtract(Set(state.displayedAccounts)).sortBy(name))
     }),
     (dispatch: (a: any) => any) => ({
         add: (a: Address) =>
