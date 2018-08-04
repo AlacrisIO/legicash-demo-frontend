45d2ec85325e879468a2d46605916afe66702452
Alex Coventry <alx@mit.edu>
2018-08-04 13:28:01 -0400

    Fix basic server-interaction tests

--- src/server/common.test.tsx
+++ src/server/common.test.tsx
@@ -1,17 +1,19 @@
 import { get, post } from './common'
 import { addresses } from './ethereum_addresses'
 
-const serverRunning = false
+const serverRunning = true
 
 describe('server/common tests', () => {
-    it("Dummy: everything's supposed to contain one test", () => undefined)
+    if (!serverRunning) {
+        it("Dummy: everything's supposed to contain one test", () => undefined)
+    }
     if (serverRunning) {
         /* XXX: Flaky tests, which depend on the server running. */
         it('Hits the balances endpoint sensibly', () =>
-            expect(get('balances', {}).then(r => { throw JSON.stringify(r) })).resolves.toBe(200))
+            expect(get('balances', {})).resolves.toEqual([]))
         it('Gets a single balance', () =>
             expect(post('balance',
-                { address: addresses.Alice.toString() }).then(
-                    r => { throw JSON.stringify(r) } /* r.status */)).resolves.toBe(200))
+                { address: addresses.Alice.toString() }))
+                .resolves.toEqual({ error: "Not_found" }))
     }
 })
--- src/server/common.tsx
+++ src/server/common.tsx
@@ -1,20 +1,30 @@
 /** The port on which the legicash server is listening */
 const endpointPort = 8081
 
+/** The hostname for the faciilitator service */
+// XXX: Note that this is not the mandated hostname. It is set to this to avoid
+// CORS errors during testing, in /etc/hosts. A better value might be
+// `window.location.hostname`?
+const hostname = "app.legi.cash"
+
 /** URL at which the legicash server is listening */
 export const serverURL =
-    `${window.location.protocol}//${window.location.hostname}:${endpointPort}`
+    `${window.location.protocol}//${hostname}:${endpointPort}`
 
 /** URL for given legicash endpoint */
 export const endpointURL = (endpoint: string) => `${serverURL}/api/${endpoint}`
 
 /** Post body to legicash endpoint  */
-export const post = (endpoint: string, body: object): Promise<Response> =>
-    fetch(endpointURL(endpoint), {
+export const post = (endpoint: string, body: object): Promise<Response | void> => {
+    return fetch(endpointURL(endpoint), {
         body: JSON.stringify(body),
         headers: { "Content-Type": "application/json; charset=utf-8" },
         method: "POST",
     })
+        .then(rv => rv.json())
+        /* tslint:disable:no-console */
+        .catch(e => { console.log(`We got an error: ${e}`); throw e })
+}
 
 /** Post get query to legicash endpoint */
 export const get = (endpoint: string, params: object): Promise<Response> => {
@@ -22,6 +32,8 @@ export const get = (endpoint: string, params: object): Promise<Response> => {
     /* Construct `?k=v&k'=v'&...` query from parameters */
     Object.keys(params).forEach(k => url.searchParams.append(k, params[k]))
     return fetch(url.toString())
+        .then(r => r.json())
+        .catch(e => { console.log(`We got an error: ${e}`); throw e })
 }
 
 export interface IThreadResponse { result: string | object }
@@ -39,4 +51,3 @@ export const readThread = (response: IThreadResponse): number => {
 export const pendingResult = { result: "The operation is pending" }
 export const resultPending = (r: IThreadResponse) =>
     r.result === pendingResult.result
-
