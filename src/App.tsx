129f452d58045af1cd58ba2a8e89995bf03a19f4
Alex Coventry <alx@mit.edu>
2018-08-09 13:06:14 -0400

    Wired redux-saga middleware into front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'  // Does not work with braces!!
 import { AddAccount } from './components/add_account'
 import { WalletList } from './components/wallet_list'
 import { rootReducer } from './reducers'
+import { depositListener } from './sagas'
 import { UIState } from './types/state'
 
-const initialState = new UIState({})
-export const store = createStore(rootReducer, initialState)
+const sagaMiddleware = createSagaMiddleware()
 
-class App extends React.Component {
+const initialState = new UIState({})
+export const store = createStore(rootReducer, initialState,
+    applyMiddleware(sagaMiddleware))
+
+/* This MUST be run after the app has been rendered. Hence the thunk */
+export const startMiddleware = () => {
+    sagaMiddleware.run(depositListener)
+}
+
+export class App extends React.Component {
     public render() {
         return (
             <Provider store={store}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { App, startMiddleware } from './App';
 import './index.css';
 /* tslint:disable:ordered-imports */
 import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+startMiddleware()
 registerServiceWorker();
