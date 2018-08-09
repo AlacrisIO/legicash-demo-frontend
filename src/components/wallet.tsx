00b4b752d564b62220d861f80d74145e2111990d
Alex Coventry <alx@mit.edu>
2018-08-09 12:27:29 -0400

    Add tx list to wallet, wire wallet up to redux

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -2,29 +2,35 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { makeDeposit, removeWallet } from '../types/actions'
 import { Address } from '../types/address'
+import { Guid } from '../types/guid'
 import { UIState } from '../types/state'
-// import { TxList } from './tx_list'  // Will need this later...
+import { Transaction } from '../types/tx'
 import { Wallet as WalletType } from '../types/wallet'
+import { TxList } from './tx_list'  // Will need this later...
 import { DepositDialog } from './txs/deposit_dialog'
 
 interface IWallet {
     depositCallback: (amount: number) => void
     killCallback: () => void;
     wallet: WalletType;
+    txs: Transaction[];
 }
-export const DumbWallet = ({ depositCallback, killCallback, wallet }: IWallet) =>
-    <div>
-        <h2>
-            <button onClick={killCallback}>⌫</button> { /* Delete affordance */}
-            Username: {wallet.username}
-        </h2>
-        <p>Address: {wallet.address.toString()}</p>
-        <DepositDialog from={wallet.address} submitCallback={depositCallback} />
-
-        <p>Offchain balance: {wallet.offchainBalance}</p>
-        <p>Onchain balance: {wallet.onchainBalance}</p>
-        <p>Transaction list: To be completed</p>
-    </div>
+export const DumbWallet =
+    ({ depositCallback, killCallback, wallet, txs }: IWallet) =>
+        <div>
+            <h2>
+                { /* Clicking on this deletes display of the wallet */}
+                <button onClick={killCallback}>⌫</button>
+                Username: {wallet.username}
+            </h2>
+            <p>Address: {wallet.address.toString()}</p>
+            <DepositDialog from={wallet.address}
+                submitCallback={depositCallback} />
+            <p>Offchain balance: {wallet.offchainBalance}</p>
+            <p>Onchain balance: {wallet.onchainBalance}</p>
+            <p>Transaction list:</p>
+            <TxList txs={txs} />
+        </div>
 
 export const Wallet =
     ({ address }: { address: Address }) =>
@@ -35,10 +41,16 @@ export const Wallet =
                     throw Error(`Wallet for unknown address! \
 ${address}`)
                 }
-                return { wallet }
+                const txs: Transaction[] = wallet.txs.map(
+                    (g: Guid) => state.txByGUID.get(g)).toArray()
+                return { txs, wallet }
             },
             (dispatch: (a: any) => any) => ({
-                depositCallback: (a: number) => dispatch(makeDeposit(address, a)),
+                depositCallback: (a: number) => {
+                    /* tslint:disable:no-console */
+                    console.log('Depositing', a)
+                    dispatch(makeDeposit(address, a))
+                },
                 killCallback: () => dispatch(removeWallet(address)),
             })
         )(DumbWallet) 
