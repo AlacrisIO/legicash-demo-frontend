d65717008c8ecef11f056054e230f50e6a0b36bc
Alex Coventry <alx@mit.edu>
2018-08-10 19:38:20 -0400

    Payment actions

---
+++ src/types/actions/payments.tsx
@@ -0,0 +1,39 @@
+import { Address } from '../address'
+import { isSideChain, makeSideChain } from '../chain'
+import { Transaction } from '../tx'
+import { Action, IActionType } from './base_actions'
+
+export interface IPaymentAction extends IActionType {
+    readonly tx: Transaction
+}
+
+/* tslint:disable:no-empty-interface */
+
+export interface IPaymentInitiated extends IPaymentAction { /* empty */ }
+export const payment = (tx: Transaction): IPaymentInitiated => {
+    if (!isSideChain(tx)) { throw Error(`Not a sidechain payment! ${tx}`) }
+    return { type: Action.PAYMENT_INITIATED, tx }
+}
+export const makePayment =
+    (to: Address, from: Address, amount: number): IPaymentInitiated =>
+        payment(makeSideChain(new Transaction({ to, from, amount })))
+
+export interface IPaymentValidated extends IPaymentAction {
+    fromBalance: number
+    toBalance: number
+}
+export const paymentValidated =
+    (tx: Transaction, fromBalance: number, toBalance: number) => {
+        if ((!tx.validated) || tx.rejected) { throw Error(`Invalid payment! ${tx}`) }
+        return { fromBalance, toBalance, tx, type: Action.PAYMENT_VALIDATED }
+    }
+
+export interface IPaymentFailed extends IPaymentAction {
+    error: Error
+}
+export const paymentFailed = (tx: Transaction, error: Error) => {
+    if (tx.validated || (!tx.rejected)) {
+        throw Error(`Unrejected payment! ${tx}`)
+    }
+    return { error, tx, type: Action.PAYMENT_FAILED }
+}
