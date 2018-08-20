e0294690fd7904df7ee624233b49cabcfff795d0
Alex Coventry <alx@mit.edu>
2018-08-20 14:41:06 -0400

    Better checking of server response in live balance test

--- src/server/common.test.tsx
+++ src/server/common.test.tsx
@@ -3,6 +3,10 @@ import { addresses } from './ethereum_addresses'
 
 const serverRunning = true
 
+export const validateBalance = (resp: any) =>
+    /0x[0-9A-Fa-f]{40}/.exec(resp.address) && (resp.balance >= 0)
+    && (resp.user_name in addresses)
+
 describe('server/common tests', () => {
     if (!serverRunning) {
         it("Dummy: everything's supposed to contain one test", () => undefined)
@@ -10,10 +14,14 @@ describe('server/common tests', () => {
     if (serverRunning) {
         /* XXX: Flaky tests, which depend on the server running. */
         it('Hits the balances endpoint sensibly', () =>
-            expect(get('balances', {})).resolves.toEqual([]))
-        it('Gets a single balance', () =>
-            expect(post('balance',
-                { address: addresses.Alice.toString() }))
-                .resolves.toEqual({ error: "Not_found" }))
+            expect(get('balances', {})
+                .then(r => (r as any).every(validateBalance)))
+                .resolves.toBeTruthy())
+        it('Gets a single balance', () => {
+            const aliceReq = { address: addresses.Alice.toString() }
+            expect(post('balance', aliceReq).then(r =>
+                validateBalance(r) || (r as any).error === 'Not_found'))
+                .resolves.toBeTruthy()
+        })
     }
 })
