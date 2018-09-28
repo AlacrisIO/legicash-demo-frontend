c4550e1565c51159a497f63e8b481798d3aaa9b8
Alex Coventry <alx@mit.edu>
2018-09-28 14:19:40 -0400

    Revise payments to parse new API response

--- src/sagas/payment.tsx
+++ src/sagas/payment.tsx
@@ -1,46 +1,40 @@
 import { call, put } from 'redux-saga/effects'
 import { post } from '../server/common'
 import * as Actions from '../types/actions'
-import { Address } from '../types/address'
 import { listener } from './common'
+
+const parseHexAsNumber = (s: string) => parseInt(s, 16)
 
 export function* payment(action: Actions.IPaymentInitiated) {
     const tx = action.tx
     try {
         const body = {
-            amount: tx.amount,
+            amount: '0x' + (tx.amount as number).toString(16),
             recipient: `${tx.to}`, sender: `${tx.from}`,
         }
         const response = yield call(post, 'payment', body)
         if (response === undefined) {
             return yield put(Actions.paymentFailed(tx.set('rejected', true),
-                                                   Error("Server failure!")))
+                Error("Server failure!")))
         }
         if (response.amount_transferred === undefined) {
             return yield put(
                 Actions.paymentFailed(tx.set('rejected', true),
-                                      Error(`Unexpected server response!
+                    Error(`Unexpected server response!
 ${JSON.stringify(response)}`)))
         }
-        if (response.amount_transferred !== tx.amount) {
+        if (response.amount_transferred !== '0x' + (tx.amount as number).toString(16)) {
             throw Error(`Server sent  a different amount than we requested!
 It sent ${response.amount_transferred}, but we requested ${tx.amount} in ${tx}`)
         }
-        const serverFrom = new Address(response.sender_account.address)
-        if (!tx.from.equals(serverFrom)) {
-            throw Error(`Server registered tx to different sender!
-It has the sender as ${serverFrom}, but we had it as ${tx.from}.`)
-        }
-        const serverTo = new Address(response.recipient_account.address)
-        if (!tx.to.equals(serverTo)) {
-            throw Error(`Server registered tx to different sender!
-It has the sender as ${serverTo}, but we had it as ${tx.to}.`)
-        }
         const fromBalance = response.sender_account.balance
         const toBalance = response.recipient_account.balance
-        // XXX: Track side_chain_tx_revision in the client??
-        yield put(Actions.paymentValidated(
-            tx.set('validated', true), fromBalance, toBalance))
+        const srcSideChainRevision = parseHexAsNumber(response.side_chain_tx_revision)
+        const dstSideChainRevision = srcSideChainRevision
+        const finalTx = tx.set('validated', true)
+            .set('srcSideChainRevision', srcSideChainRevision)
+            .set('dstSideChainRevision', dstSideChainRevision)
+        yield put(Actions.paymentValidated(finalTx, fromBalance, toBalance))
     }
     catch (e) {
         throw e
