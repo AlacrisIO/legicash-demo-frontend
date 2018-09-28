5ebaae75cc4bc4896e68b85dd4012eebc626f144
Alex Coventry <alx@mit.edu>
2018-09-28 14:19:18 -0400

    Revise recent_transactions to parse new API response

--- src/sagas/recent_transactions.tsx
+++ src/sagas/recent_transactions.tsx
@@ -2,66 +2,103 @@ import { call, put } from 'redux-saga/effects'
 import { post } from '../server/common'
 import * as Actions from '../types/actions'
 import { Address } from '../types/address'
-import {
-    depositTransaction, makeSideChain, withdrawTransaction
-} from '../types/chain'
+import * as Chain from '../types/chain'
 import { Transaction } from '../types/tx'
 import { listener } from './common'
 
+type hex_string = string
+type hex_number = hex_string
+
+const parseHexAsNumber = (h: hex_number) => parseInt(h, 16)
+
+interface IDeposit {
+    deposit_amount: hex_number,
+    deposit_fee: hex_number,
+    main_chain_deposit: { tx_header: { sender: hex_string } }
+}
+
+/* tslint:disable:no-empty-interface */
+interface IPayment {
+    payment_invoice: { recipient: Address, amount: hex_number, memo: string },
+}
+
+interface IWithdrawal { /* empty */ }
+
+type DepositOperation = ["Deposit", IDeposit]
+type WithdrawalOperation = ["Withdrawal", IWithdrawal]
+type PaymentOperation = ["Payment", IPayment]
+type Operation = DepositOperation | WithdrawalOperation | PaymentOperation
+
 interface IResponse {
-    amount: number
-    facilitator_revision: number
-    transaction_type: 'deposit' | 'payment' | 'withdrawal'
+    tx_header: { tx_revision: hex_number, updated_limit: hex_number },
+    tx_request: ["UserTransaction", {
+        payload: {
+            operation: Operation
+            rx_header: { requester_revision: hex_number, requester: Address }
+        }
+    }]
 }
 
-interface IDepositOrWithdrawalResponse extends IResponse {
-    address: string
+// XXX: Move sidechain update logic into ../types/chain.tsx
+
+export const txFromDeposit = (d: IResponse): Transaction => {
+    const payload = d.tx_request[1].payload
+    if (payload.operation[0] !== "Deposit") {
+        throw Error(`txFromDeposit called with operation which is not a deposit!
+${payload.operation}`)
+    }
+    const deposit = payload.operation[1] as IDeposit
+    const address = new Address(deposit.main_chain_deposit.tx_header.sender)
+    const amount = parseHexAsNumber(deposit.deposit_amount)
+    const dstSideChainRevision = parseHexAsNumber(
+        payload.rx_header.requester_revision)
+    return new Transaction({
+        amount, dstSideChainRevision, from: address, to: address
+    })
 }
 
-interface IPaymentResponse extends IResponse {
-    sender: string
-    recipient: string
+export const txFromWithdrawal = (d: IResponse): Transaction => {
+    const payload = d.tx_request[1].payload
+    if (payload.operation[0] !== "Withdrawal") {
+        throw Error(`txFromWithdrawal called with operation which is not a withdrawal!
+${payload.operation}`)
+    }
+    const withdrawal = payload.operation[1] as IWithdrawal
+    JSON.stringify(withdrawal)  // Shut linter up
+    throw Error("Not implemented")
 }
-
-// Withdrawals have a negative amount
-export const txFromDepositOrWithdrawal = (d: IDepositOrWithdrawalResponse
-): Transaction =>
-    (d.amount > 0 ? depositTransaction : withdrawTransaction)(
-        new Address(d.address), Math.abs(d.amount))
-
-export const txFromDeposit = (d: IDepositOrWithdrawalResponse
-): Transaction => txFromDepositOrWithdrawal(d)
-    .set('dstSideChainRevision', d.facilitator_revision)
-
-export const txFromWithdrawal = (d: IDepositOrWithdrawalResponse
-): Transaction => txFromDepositOrWithdrawal(d)
-    .set('srcSideChainRevision', d.facilitator_revision)
 
 /* tslint:disable:object-literal-sort-keys */
-export const txFromPayment = (p: IPaymentResponse): Transaction =>
-    makeSideChain(new Transaction({
-        amount: p.amount,
-        from: new Address(p.sender),
-        to: new Address(p.recipient.startsWith('0x') ?
-            p.recipient : '0x' + p.recipient),
-        srcSideChainRevision: p.facilitator_revision,
-        dstSideChainRevision: p.facilitator_revision,
-    }))
-
-export const txFromResponse = (r: IResponse): Transaction => {
-    let tx: Transaction
-    switch (r.transaction_type) {
-        case 'deposit': tx = txFromDeposit(r as IDepositOrWithdrawalResponse)
-            break
-        case 'withdrawal': tx = txFromWithdrawal(
-            r as IDepositOrWithdrawalResponse)
-            break
-        case 'payment': tx = txFromPayment(r as IPaymentResponse)
-            break
-        default: throw Error(`Unable to process tx ${JSON.stringify(r)}`)
+export const txFromPayment = (p: IResponse): Transaction => {
+    const payload = p.tx_request[1].payload
+    if (payload.operation[0] !== "Payment") {
+        throw Error(`txFromPayment called with operation which is not a payment!
+${payload.operation}`)
     }
-    return tx.set('validated', true)
+    const payment = payload.operation[1] as IPayment
+    const amount = parseHexAsNumber(payment.payment_invoice.amount)
+    const from = new Address(payload.rx_header.requester)
+    const dstSideChainRevision = parseHexAsNumber(p.tx_header.tx_revision)
+    const srcSideChainRevision = dstSideChainRevision
+    const to = new Address(payment.payment_invoice.recipient)
+    return new Transaction({
+        amount, dstSideChainRevision, srcSideChainRevision, from, to,
+        srcChain: Chain.Chain.Side, dstChain: Chain.Chain.Side
+    })
+    throw Error("Not implemented")
 }
+
+const responseMap = {
+    "Deposit": txFromDeposit,
+    "Withdrawal": txFromWithdrawal,
+    "Payment": txFromPayment
+}
+
+/** Parse a tx from the server response for this transaction. */
+export const txFromResponse = (r: IResponse): Transaction => (
+    responseMap[r.tx_request[1].payload.operation[0]](r)
+        .set('validated', true)
+)
 
 export function* recentTxs(action: Actions.IRecentTxsInitiated) {
     const address = action.address.toString()
