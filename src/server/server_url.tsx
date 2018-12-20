6cd107c98f9a5a087f2df94329c4f887c4f59493
Luka Defar <luka.defar@rocket-internet.de>
2018-12-21 00:28:52 +0100

    Semantic UI  - staging server url

--- src/server/server_url.tsx
+++ src/server/server_url.tsx
@@ -1,13 +1,13 @@
 /** Blow this file away and put "serverURL = <my URL>" */
 
 /** The port on which the legicash server is listening */
-const endpointPort = 8081
+const endpointPort = 80
 
 /** The hostname for the faciilitator service */
 // XXX: Note that this is not the mandated hostname. It is set to this to avoid
 // CORS errors during testing, in /etc/hosts. A better value might be
 // `window.location.hostname`?
-const hostname = "app.legi.cash"
+const hostname = "alacris-client.flyingpenguin.tech"
 
 /** URL at which the legicash server is listening */
 export const serverURL =
