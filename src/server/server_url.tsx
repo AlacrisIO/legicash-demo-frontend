84384db93b6ff4bd4f53ad133afab59145d098ba
Luka Defar <luka.defar@rocket-internet.de>
2019-01-02 20:02:02 +0100

    Enviromental config for api host and port

--- src/server/server_url.tsx
+++ src/server/server_url.tsx
@@ -1,13 +1,22 @@
 /** Blow this file away and put "serverURL = <my URL>" */
 
 /** The port on which the legicash server is listening */
-const endpointPort = 80
+let endpointPort = 80
 
 /** The hostname for the faciilitator service */
 // XXX: Note that this is not the mandated hostname. It is set to this to avoid
 // CORS errors during testing, in /etc/hosts. A better value might be
 // `window.location.hostname`?
-const hostname = "alacris-client.flyingpenguin.tech"
+let hostname = "alacris-client.flyingpenguin.tech"
+
+// .env overrides
+if ( process.env.REACT_APP_API_HOST ) {
+    hostname = process.env.REACT_APP_API_HOST;
+}
+
+if ( process.env.REACT_APP_API_PORT ) {
+    endpointPort = (process.env.REACT_APP_API_PORT as any as number);
+}
 
 /** URL at which the legicash server is listening */
 export const serverURL =
