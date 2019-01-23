4db631fd8136aaa35d72b922d8f4fcb0ea8e4457
Matt Audesse <matt@mattaudesse.com>
2019-01-23 11:31:28 -0500

    Fix broken server/common tests when running against backend @ eb91650

--- src/server/common.test.tsx
+++ src/server/common.test.tsx
@@ -1,31 +1,53 @@
 import { get, post } from './common'
 import { addresses } from './ethereum_addresses'
 
-const serverRunning = true
-
 export const validateBalance = (resp: any) => {
     if (!(parseInt(resp.balance, 16) >= 0
         && parseInt(resp.account_revision, 16) >= 0)) {
-        throw Error(`Bad balance response: ${resp}`)
+
+        throw Error(`Bad balance response: ${JSON.stringify(resp)}`)
     }
+
     return true
 }
 
+const allWith = (elems, f) =>
+    elems.reduce((acc, e) => acc && f(e), true)
+
 describe('server/common tests', () => {
-    if (!serverRunning) {
-        it("Dummy: everything's supposed to contain one test", () => undefined)
-    }
-    if (serverRunning) {
-        /* XXX: Flaky tests, which depend on the server running. */
-        it('Hits the balances endpoint sensibly', () =>
-            expect(get('balances', {})
-                .then(r => (r as any).every(validateBalance)))
-                .resolves.toBeTruthy())
-        it('Gets a single balance', () => {
-            const aliceReq = { address: addresses.Alice.toString() }
-            expect(post('balance', aliceReq).then(r =>
-                validateBalance(r) || (r as any).error === 'Not_found'))
-                .resolves.toBeTruthy()
-        })
-    }
+    /* XXX: Flaky tests which depend on the server running. */
+    let connected = false
+
+    const whenConnectedIt = (description, spec) =>
+        it(description, () => connected
+            ? spec()
+            : expect(connected).toBe(true));
+
+    // NB 2019-01-22: our present CORS config prevents us using a more generic
+    // route such as GET /, so we're picking GET /api/balances out of the hat
+    // here instead - we just need to know whether the server is responding
+    beforeAll(() => get('balances', {})
+        .then(r => connected = true ))
+
+    whenConnectedIt('Hits the balances endpoint sensibly', () => {
+        const req = get('balances', {})
+            .then(r => allWith(Object.values(r), validateBalance))
+
+        return expect(req)
+            .resolves.toBe(true)
+    })
+
+    whenConnectedIt('Gets a single balance', () => {
+        const address     = addresses.Alice.toString()
+        const notFoundMsg = `Could not find balance for address ${address}`
+
+        const req = post('balance', { address })
+            .then(r => r.error
+                ? r.error   === notFoundMsg
+                : r.address === address && Number.isFinite(
+                    parseInt(r.account_balance, 16)))
+
+        return expect(req)
+            .resolves.toBe(true)
+    })
 })
