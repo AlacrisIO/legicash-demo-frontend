6a30b4e96c0096524954c42f000ecef859cc643f
Luka Defar <luka.defar@rocket-internet.de>
2019-02-04 13:51:17 +0100

    String Enums for Actions

--- src/types/actions/base_actions.tsx
+++ src/types/actions/base_actions.tsx
@@ -5,36 +5,36 @@ import { ServerResponse } from './server'
 export enum Action {
 
     /* Actions related to making a deposit. */
-    MAKE_DEPOSIT, // Deposit to this address on side chain
-    DEPOSIT_VALIDATED, // Deposit has been added
-    DEPOSIT_FAILED, // Deposit was not added
+    MAKE_DEPOSIT = 'MAKE_DEPOSIT', // Deposit to this address on side chain
+    DEPOSIT_VALIDATED = 'DEPOSIT_VALIDATED', // Deposit has been added
+    DEPOSIT_FAILED = 'DEPOSIT_FAILED', // Deposit was not added
 
     /* Actions related to making a deposit. */
-    MAKE_WITHDRAWAL, // Withdrawal to this address on side chain
-    WITHDRAWAL_VALIDATED, // Withdrawal has been added
-    WITHDRAWAL_FAILED, // Deposit was not added
+    MAKE_WITHDRAWAL = 'MAKE_WITHDRAWAL', // Withdrawal to this address on side chain
+    WITHDRAWAL_VALIDATED = 'WITHDRAWAL_VALIDATED', // Withdrawal has been added
+    WITHDRAWAL_FAILED = 'WITHDRAWAL_FAILED', // Deposit was not added
 
-    PAYMENT_INITIATED,  // Side-chain cross-account payment
-    PAYMENT_VALIDATED,
-    PAYMENT_FAILED,
+    PAYMENT_INITIATED = 'PAYMENT_INITIATED',  // Side-chain cross-account payment
+    PAYMENT_VALIDATED = 'PAYMENT_VALIDATED',
+    PAYMENT_FAILED = 'PAYMENT_FAILED',
 
     /* Actions related to querying the facalitator state */
-    BALANCES_OBSERVED,  // Response from `balances` API endpoint.
+    BALANCES_OBSERVED = 'BALANCES_OBSERVED',  // Response from `balances` API endpoint.
 
-    RECENT_TRANSACTIONS_INITIATED,  // Request for recent txs
-    RECENT_TRANSACTIONS_RECEIVED,  // Request for recent txs
-    RECENT_TRANSACTIONS_FAILED,  // Request for recent txs
-    RECENT_TRANSACTIONS_NEW_PAYMENTS,  // New payments to wallet, launch snackbar
+    RECENT_TRANSACTIONS_INITIATED = 'RECENT_TRANSACTIONS_INITIATED',  // Request for recent txs
+    RECENT_TRANSACTIONS_RECEIVED = 'RECENT_TRANSACTIONS_RECEIVED',  // Request for recent txs
+    RECENT_TRANSACTIONS_FAILED = 'RECENT_TRANSACTIONS_FAILED',  // Request for recent txs
+    RECENT_TRANSACTIONS_NEW_PAYMENTS = 'RECENT_TRANSACTIONS_NEW_PAYMENTS',  // New payments to wallet, launch snackbar
 
-    PROOF_TOGGLED,  // Display a Merkle proof
-    PROOF_REQUESTED,  // Request for a Merkle proof
-    PROOF_RECEIVED_AND_VALID,  // Valid proof has been returned
-    PROOF_RECEIVED_BUT_INVALID,  // Invalid proof has been returned!
-    PROOF_ERROR,  // Error during proof retrieval / validation
+    PROOF_TOGGLED = 'PROOF_TOGGLED',  // Display a Merkle proof
+    PROOF_REQUESTED = 'PROOF_REQUESTED',  // Request for a Merkle proof
+    PROOF_RECEIVED_AND_VALID = 'PROOF_RECEIVED_AND_VALID',  // Valid proof has been returned
+    PROOF_RECEIVED_BUT_INVALID = 'PROOF_RECEIVED_BUT_INVALID',  // Invalid proof has been returned!
+    PROOF_ERROR = 'PROOF_ERROR',  // Error during proof retrieval / validation
 
     /* UI actions */
-    ADD_WALLET, // Add this address to the list of displayed wallets
-    REMOVE_WALLET,  // Remove this address from the list of displayed wallets.
+    ADD_WALLET = 'ADD_WALLET', // Add this address to the list of displayed wallets
+    REMOVE_WALLET = 'REMOVE_WALLET',  // Remove this address from the list of displayed wallets.
 
 }
 
