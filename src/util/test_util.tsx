a2ad580e192eea0b9c0f6c1544e67e0d56b30807
Matt Audesse <matt@mattaudesse.com>
2019-04-19 10:30:46 -0400

    Move `whenConnectedIt` combinator to src/types/tx.test.tsx

--- src/util/test_util.tsx
+++ src/util/test_util.tsx
@@ -1,8 +1,23 @@
-import {ReactWrapper} from "enzyme";
-import {addresses} from "../server/ethereum_addresses";
+import { ReactWrapper } from  'enzyme'
+import { get }          from  '../server/common'
+import { addresses }    from  '../server/ethereum_addresses'
 
-export const findUserSelectItem = (m: ReactWrapper, n: string) => m.find('div.item').filterWhere((c: any): boolean => {
-    return c.find('span.text') && c.find('span.text').html() === `<span class="text">${n}</span>`;
-});
+export const findUserSelectItem = (m: ReactWrapper, n: string) =>
+    m.find('div.item')
+     .filterWhere((c: any): boolean =>
+           c.find('span.text')
+        && c.find('span.text').html() === `<span class="text">${n}</span>`);
 
 export const findAddressByName = (n: string) => addresses[n];
+
+// NB 2019-01-22: our present CORS config prevents us using a more generic
+// route such as GET /, so we're picking GET /api/balances out of the hat
+// here instead - we just need to know whether the server is responding
+let connected = false
+beforeAll(() => get('balances', {})
+    .then(r => connected = true ))
+
+export const whenConnectedIt = (description: string, spec: () => Promise<any>) =>
+    it(description, () => connected
+        ? spec()
+        : expect(connected).toBe(true));
