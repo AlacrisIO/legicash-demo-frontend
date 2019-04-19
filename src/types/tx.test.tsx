9b191029f3a8d484910fbb5f110487f8806574ad
Matt Audesse <matt@mattaudesse.com>
2019-04-19 10:31:45 -0400

    Fix formatting, broken tests in src/types/tx.test.tsx

--- src/types/tx.test.tsx
+++ src/types/tx.test.tsx
@@ -1,17 +1,26 @@
-import { Address } from './address'
-import { Chain } from './chain'
-import { HashValue } from './hash'
-import { Transaction } from './tx'
-import {Money} from "./units";
+import { Address }     from  './address'
+import { Chain }       from  './chain'
+import { HashValue }   from  './hash'
+import { Transaction } from  './tx'
+import { Money }       from  './units'
 
 const addr1 = new Address('0x5050505050505050505050505050505050505050')
 const addr2 = new Address('0x0505050505050505050505050505050505050505')
+
 const hash = new HashValue(
     '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
-const defaultTransaction = {
-    amount: new Money('1'), dstChain: Chain.Side, failureMessage: '', from: addr1,
-    hash, rejected: undefined, srcChain: Chain.Main, to: addr2, validated: false,
-}
+
+const defaultTransaction =
+    { amount:         new Money('1')
+    , dstChain:       Chain.Side
+    , failureMessage: ''
+    , from:           addr1
+    , hash
+    , rejected:       undefined
+    , srcChain:       Chain.Main
+    , to:             addr2
+    , validated:      false
+    }
 
 describe('Tests of Transaction type', () => {
     it('Accepts and stores a sensible transaction', () => {
@@ -20,7 +29,7 @@ describe('Tests of Transaction type', () => {
     })
 
     it('Rejects transactions with a negative amount', () => {
-        expect(() => new Transaction({ ...defaultTransaction, amount: new Money('0') }))
+        expect(() => new Transaction({ ...defaultTransaction, amount: new Money('-1') }))
             .toThrow()
     })
 
