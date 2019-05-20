e41c584754d5c8fa607d5bc04888566da74bc030
Matt Audesse <matt@mattaudesse.com>
2019-05-20 11:49:37 -0400

    Improve src/sagas/proofs.tsx legibility

--- src/sagas/proofs.tsx
+++ src/sagas/proofs.tsx
@@ -1,28 +1,32 @@
-import { call, put } from 'redux-saga/effects'
-import { get } from '../server/common'
-import * as Actions from '../types/actions'
-import { sidechainIdx } from '../types/chain'
+import { call, put }                from 'redux-saga/effects'
+import { get }                      from '../server/common'
+import * as Actions                 from '../types/actions'
+import { sidechainIdx }             from '../types/chain'
 import { stepsIntermediateDigests } from '../types/proofs/proof_protocol'
-import { IResponse } from '../types/proofs/proof_types'
-import { listener } from './common'
+import { IResponse }                from '../types/proofs/proof_types'
+import { listener }                 from './common'
 
 export function* proof(action: Actions.IProofRequested) {
     const id = sidechainIdx(action.tx)
     try {
         const r = yield call(get, 'proof', { 'tx-revision': id })
+
         if (r === undefined) {
             return yield put(
                 Actions.proofError(action.tx, Error("Server failure!")))
         }
+
         if (r.steps && r.leaf) {
             // XXX: Constructing a "valid" proof without validation!!!
             const validationSteps = stepsIntermediateDigests(r.steps, r.leaf)
             return yield put(Actions.proofReceivedAndValid(
                 action.tx, { ...(r as IResponse), validationSteps }))
+
         } else {
             return yield put(Actions.proofReceivedButInvalid(
                 action.tx, r as IResponse))
         }
+
     } catch (e) {
         return yield put(Actions.proofError(action.tx, e))
     }
